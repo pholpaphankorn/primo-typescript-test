fd7cd937a582043b59f6c4132b72de496480397e
pholpaphankorn <phol12711@outlook.com>
2026-05-10 16:02:22 +0700

    feat: add entry point

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { merge } from './merge';
+
+const c1 = [15, 10, 5];
+const c2 = [1, 3, 5];
+const c3 = [2, 4, 6];
+
+const result = merge(c1, c2, c3);
+
+console.log("--- Merge Result ---");
+console.log(result);
